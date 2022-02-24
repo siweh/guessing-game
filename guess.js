6735338e0a8647ddfe47783071533b82ead0c17c
siweh <sanemadesi@gmail.com>
2022-02-24 11:33:33 +0200

    Added more functionality

--- guess.js
+++ guess.js
@@ -3,22 +3,32 @@ let randomNumber = Math.ceil(Math.random() * 100);
 let results = document.querySelector('.results');
 
 let submitGuess = document.querySelector('.guessSubmit');
+let newNumber = document.querySelector('.newNumber');
 let guessField = document.querySelector('.guessField');
+let error = document.querySelector('.error');
 
 //results.innerHTML = randomNumber;
 function guessedNumber() {
   let guessedValue = guessField.value;
   results.innerHTML = guessedValue;
-  if (guessedValue < randomNumber) {
+
+  if (guessedValue < 1 || guessedValue > 100) {
+    error.innerHTML = 'Please enter a number from 1 up to 100 only!';
+    results.innerHTML = '';
+  } else if (guessedValue < randomNumber) {
     results.innerHTML = 'Your guess is too low';
   } else if (guessedValue > randomNumber) {
     results.innerHTML = 'Your guess is too high';
   } else {
     results.innerHTML = `Correct, the secret number is ${randomNumber}`;
+    randomNumber = Math.ceil(Math.random() * 100);
+    newNumber.innerHTML = 'A new game started!';
+    guessField.value = '';
   }
 
   setTimeout(() => {
     results.innerHTML = '';
-  }, 3000);
+    error.innerHTML = '';
+  }, 5000);
 }
 submitGuess.addEventListener('click', guessedNumber);
